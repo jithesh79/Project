4850434cb1428cf02cf4023052b2906ff6fb04d3
jithesh79 <jitheshks11@gmail.com>
2025-11-24 09:55:17 +0530

    first commit

---
+++ Hands_project/New folder/terraform/modules/rds/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
